4eede11e275db721099c552ad52f4e6d1dbb0943
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2025-12-14 23:48:32 +0530

    Add functionality to male button for selecting color

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var mybotton = document.getElementById("male");
+var radiomale = document.getElementById("red");
+
+mybotton.onclick = function() {
+    radiomale.checked = true;
+}
+
+
+
+
 function bmicalculator(weight, height) {
     var bmi = weight / (height * height);
     return bmi;
